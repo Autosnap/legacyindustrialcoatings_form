552c3a6c566cd5a30ebdc302b8e92129cd6f1919
Autosnap <DANGARDNER@AUTOSNAPLIVE.COM>
2024-08-23 10:33:38 -0500

    Updates

---
+++ precache-manifest.c92ad5d2eab45d17db489954b74e4ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa4fde7b540724ecf59a5b01db749da",
+    "url": "/Autosnap/legacyindustrialcoatings_form/index.html"
+  },
+  {
+    "revision": "10d2f9ebe5eb583ea1f0",
+    "url": "/Autosnap/legacyindustrialcoatings_form/static/css/2.12f8b395.chunk.css"
+  },
+  {
+    "revision": "565f434cdd6d392f4ffa",
+    "url": "/Autosnap/legacyindustrialcoatings_form/static/css/main.56805089.chunk.css"
+  },
+  {
+    "revision": "10d2f9ebe5eb583ea1f0",
+    "url": "/Autosnap/legacyindustrialcoatings_form/static/js/2.e7298fa0.chunk.js"
+  },
+  {
+    "revision": "565f434cdd6d392f4ffa",
+    "url": "/Autosnap/legacyindustrialcoatings_form/static/js/main.b1b13c97.chunk.js"
+  },
+  {
+    "revision": "7d3a7b33125d71355968",
+    "url": "/Autosnap/legacyindustrialcoatings_form/static/js/runtime~main.255c8791.js"
+  }
+]);
